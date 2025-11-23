9c74b57a2be33f9390d6ea02419460715051a33f
Mukesh Ranjan <mukeshr@tuta.com>
2025-11-23 10:17:24 +0530

    refactored for better error handling and maintainance

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,24 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("query {}",query);
-    println!("file {}",file_path);
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read file");
+    let config = Config::new(&args);
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read file");
     println!("contents {contents}");
     dbg!(args);
 }
-fn parse_config(args: &[String])
+struct Config{
+    query: String,
+    file_path: String,
+    }
+impl Config{
+    fn build(args: &[String]) -> Result<Config, 'static &str>{
+        if args.len() < 3 {
+            Err("not enough arguments")
+            }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        println!("query {}",query);
+        println!("file {}",file_path);
+        Ok(Config{query, file_path})
+     }
+}
