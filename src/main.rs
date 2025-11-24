5f91d1b7b2f16e1edb9bac700dfb286a92187f59
Mukesh Ranjan <mukeshr@tuta.com>
2025-11-24 10:54:38 +0530

    some refactoring

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,37 @@ use std::fs;
 use std::process;
 use std::error::Error;
 use minigrep::search;
+use minigrep::search_case_insensitive;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("problem parsing arguments {err}");
+        eprintln!("problem parsing arguments {err}");
         process::exit(1);
         });
     if let Err(e) = run(config){
-        println!("{e}");
+        eprintln!("{e}");
         process::exit(1);
         };
 }
 fn run(config: Config) -> Result<(),Box<dyn Error>>{
         let contents = fs::read_to_string(config.file_path)?;
-        for line in search(&config.query, &contents){
+        let result = if config.ignore_case {
+             for line in search_case_insensitive(&config.query, &contents){
+                            println!("{line}");
+                        }
+            }
+        else{
+            for line in search(&config.query, &contents){
                 println!("{line}");
             }
+        };
         Ok(())
     }
 struct Config{
     query: String,
     file_path: String,
+    ignore_case: bool,
     }
 impl Config{
     fn build(args: &[String]) -> Result<Config, &'static str>{
@@ -33,6 +42,7 @@ impl Config{
             }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Ok(Config{query, file_path})
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config{query, file_path, ignore_case})
      }
 }
