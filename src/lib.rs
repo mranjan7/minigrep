4467f5f53f5ad24154a9949e0b0b5437de43a1d5
Mukesh Ranjan <mukeshr@tuta.com>
2025-11-24 10:04:41 +0530

    implemented search case insensitive function

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,22 @@ pub fn search<'a> (query: &str,contents: &'a str) -> Vec<&'a str>{
         }
         res
     }
+pub fn search_case_insensitive<'a> (query: &str,contents: &'a str) -> Vec<&'a str>{
+    let mut res = Vec::new();
+    let query = query.to_lowercase();
+    for line in contents.lines(){
+            if line.to_lowercase().contains(&query){
+                    res.push(line);
+                }
+        }
+        res
+    }
 #[cfg(test)]
 mod tests{
     use crate::search;
+    use crate::search_case_insensitive;
     #[test]
-    fn one_result(){
+    fn case_sensitive(){
         let query = "duct";
         let content = "\
         duct,tape
@@ -19,4 +30,14 @@ mod tests{
         ";
         assert_eq!(vec!["duct,tape"],search(query,content));
         }
+    #[test]
+    fn case_insensitive(){
+        let query = "Duct";
+        let content = "\
+        duct,tape
+        cello
+        ";
+        assert_eq!(vec!["duct,tape"],search_case_insensitive(query,content));
+        }
+
     }
