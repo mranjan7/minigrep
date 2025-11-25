b1ac3ba35b58d6f7052723e928694080a878e3be
Parrot Live user <user@localhost.localdomain>
2025-11-25 09:14:31 +0530

    some refactorings

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,9 @@
 pub fn search<'a> (query: &str,contents: &'a str) -> Vec<&'a str>{
-    let mut res = Vec::new();
-    for line in contents.lines(){
-            if line.contains(query){
-                    res.push(line);
-                }
-        }
-        res
+    contents.lines().filter(|line| line.contains(query)).collect()
     }
 pub fn search_case_insensitive<'a> (query: &str,contents: &'a str) -> Vec<&'a str>{
-    let mut res = Vec::new();
     let query = query.to_lowercase();
-    for line in contents.lines(){
-            if line.to_lowercase().contains(&query){
-                    res.push(line);
-                }
-        }
-        res
+    contents.lines().filter(|line| line.to_lowercase().contains(&query)).collect()
     }
 #[cfg(test)]
 mod tests{
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use minigrep::search_case_insensitive;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("problem parsing arguments {err}");
         process::exit(1);
         });
@@ -36,12 +36,16 @@ struct Config{
     ignore_case: bool,
     }
 impl Config{
-    fn build(args: &[String]) -> Result<Config, &'static str>{
-        if args.len() < 3 {
-            return Err("not enough arguments");
-            }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str>{
+        args.next();
+        let query = match args.next(){
+            Some(arg) => arg,
+            None => return Err("Did not get a query string"),
+            };
+        let file_path = match args.next(){
+            Some(arg) => arg,
+            None => return Err("Did not get a file path")
+            };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
         Ok(Config{query, file_path, ignore_case})
      }
